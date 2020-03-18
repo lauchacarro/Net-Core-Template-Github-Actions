47136989a83a1fbd1a107656660304a0e767e6a0
Lautaro Carro <lautaroecarro@gmail.com>
2020-03-18 00:22:46 -0300

    use new library

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-const exec = require('@actions/exec');
-const core = require('@actions/core');
-const glob = require('@actions/glob');
+const GHActionNetCore = require('gh-action-netcore');
 
-const main = async () => {
+const action = new GHActionNetCore(__dirname);
 
-    const globber = await glob.create(__dirname + '/**/*.csproj', { followSymbolicLinks: false })
-    const files = await globber.glob()
-
-    const projectPath = files[0];
-
-    let args = ['run', '-c', 'Release', '--project', projectPath, '--'];
-
-    var env = process.env;
-
-    Object.keys(env).forEach(function (envKey) {
-        if (envKey.startsWith("INPUT_")) {
-            let key = `-${envKey.replace("INPUT_", "").toLowerCase()}`;
-            args.push(key);
-            args.push(env[envKey]);
-        }
-    });
-
-
-
-
-    await exec.exec('dotnet', args);
-};
-
-main().catch(err => {
+action.start().catch(err => {
     console.error(err);
     console.error(err.stack);
     process.exit(-1);
